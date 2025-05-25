14336ea848e82875fde265692309840a11419de5
marco <marcoezequielcedro@gmail.com>
2025-05-25 16:47:12 -0300

    Primeiro Commit

---
+++ src/test/java/com/techchallenge/backend/TechChallengeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techchallenge.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechChallengeBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
